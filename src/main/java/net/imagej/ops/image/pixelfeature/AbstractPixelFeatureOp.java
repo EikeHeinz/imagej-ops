811b923a9323264eb61ca6e6a6405d060d4e2068
Eike Heinz <eike.w.heinz@gmail.com>
2015-10-30 15:30:50 +0100

    start work on framework for pixelfeatures

---
+++ src/main/java/net/imagej/ops/image/pixelfeature/AbstractPixelFeatureOp.java
@@ -0,0 +1,20 @@
+package net.imagej.ops.image.pixelfeature;
+
+import org.scijava.plugin.Parameter;
+
+import net.imagej.ops.AbstractFunctionOp;
+import net.imagej.ops.OpService;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.numeric.RealType;
+
+public abstract class AbstractPixelFeatureOp<T extends RealType<T>>
+		extends AbstractFunctionOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> {
+	
+	// TODO output might be a stack of RAIs
+	// TODO add labeling as optional parameter?
+	// TODO is this class necessary?
+
+	@Parameter
+	protected OpService ops;
+
+}
