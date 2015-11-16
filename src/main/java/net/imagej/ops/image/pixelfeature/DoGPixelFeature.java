813a7d92f2ab2d5ac9b291f93250538a06f5c920
Eike Heinz <eike.w.heinz@gmail.com>
2015-11-16 15:38:21 +0100

    remove dependency of ops parameter and update comment

--- src/main/java/net/imagej/ops/image/pixelfeature/DoGPixelFeature.java
+++ src/main/java/net/imagej/ops/image/pixelfeature/DoGPixelFeature.java
@@ -22,22 +22,22 @@ public class DoGPixelFeature<T extends RealType<T>> extends AbstractPixelFeature
 	public RandomAccessibleInterval<T> compute(RandomAccessibleInterval<T> input) {
 		
 		double temp = Math.log(maxSigma) / Math.log(2);
-		double maxSteps = ops.math().floor(temp);
+		double maxSteps = ops().math().floor(temp);
 		double[] sigmas = new double[(int) maxSteps];
 
 		for (int i = 0; i < maxSteps; i++) {
 			sigmas[i] = Math.pow(2, i) * minSigma;
 		}
 		
-		// TODO create output stack
-		RandomAccessibleInterval<T> out = (RandomAccessibleInterval<T>) ops.create().img(input.dimension(0),
+		// FIXME limited to two dimensions
+		RandomAccessibleInterval<T> out = (RandomAccessibleInterval<T>) ops().create().img(input.dimension(0),
 				input.dimension(1), input.dimension(2), (long) maxSteps);
 
 		for (int i = 0; i < maxSteps - 1; i++) {
 			for (int j = i + 1; j < maxSteps; j++) {
 				RandomAccessibleInterval<T> interval = Views.hyperSlice(Views.hyperSlice(out, 3, 1), 2, i);
 				// FIXME no subtract method for DoG found
-				ops.filter().dog(interval, input, sigmas[i], sigmas[j]);
+				ops().filter().dog(interval, input, sigmas[i], sigmas[j]);
 			}
 		}
 		return out;
