548b5de74fade6833f5dca64b72ec3563f5f7cb9
Eike Heinz <eike.w.heinz@gmail.com>
2015-12-08 15:10:09 +0100

    fix difference of gaussian pixel feature results currently not in output, see annotations

--- src/main/java/net/imagej/ops/image/pixelfeature/DoGPixelFeature.java
+++ src/main/java/net/imagej/ops/image/pixelfeature/DoGPixelFeature.java
@@ -1,21 +1,21 @@
 package net.imagej.ops.image.pixelfeature;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import org.scijava.plugin.Parameter;
-import org.scijava.plugin.Plugin;
-
-import net.imagej.ops.ComputerOp;
+import net.imagej.ops.FunctionOp;
 import net.imagej.ops.Ops;
 import net.imagej.ops.Ops.Image.DoGPxFeature;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.Views;
 
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
 @Plugin(type = Ops.Image.DoGPxFeature.class, name = Ops.Image.DoGPxFeature.NAME)
-public class DoGPixelFeature<T extends RealType<T>> extends AbstractPixelFeatureOp<T> implements DoGPxFeature {
+public class DoGPixelFeature<T extends RealType<T>> extends
+		AbstractPixelFeatureOp<T> implements DoGPxFeature {
 
 	@Parameter
 	private double minSigma;
@@ -23,40 +23,39 @@ public class DoGPixelFeature<T extends RealType<T>> extends AbstractPixelFeature
 	@Parameter
 	private double maxSigma;
 
-	// private double[] sigmas;
-
 	private double maxSteps;
 
 	private RandomAccessibleInterval<T> output;
 
-	private List<ComputerOp<RandomAccessibleInterval, RandomAccessibleInterval>> doGOps;
+	private List<FunctionOp<RandomAccessibleInterval<T>, RandomAccessibleInterval>> doGOps;
+
+	private RandomAccessibleInterval<T> extendedIn;
 
 	@Override
 	public void initialize() {
 		maxSteps = ops().math().floor(Math.log(maxSigma) / Math.log(2));
-		
+
 		long[] dims = new long[in().numDimensions() + 2];
-		for (int i = 0; i < dims.length-1; i++) {
+		for (int i = 0; i < dims.length - 1; i++) {
 			dims[i] = in().dimension(i);
 		}
-		dims[dims.length-1] = (long) maxSteps;
+		dims[dims.length - 1] = (long) maxSteps;
 
 		// FIXME replace with createOp = ops().function(Create.Img.class,
 		// RandomAccessibleInterval.class, long[].class);
 		output = (RandomAccessibleInterval<T>) ops().create().img(dims);
+		// FIXME extension necessary?
+		extendedIn = (RandomAccessibleInterval<T>) Views.interval(
+				Views.extendMirrorDouble(in()), in());
 
-		doGOps = new ArrayList<ComputerOp<RandomAccessibleInterval, RandomAccessibleInterval>>();
+		doGOps = new ArrayList<FunctionOp<RandomAccessibleInterval<T>, RandomAccessibleInterval>>();
 		for (int i = 0; i < maxSteps - 1; i++) {
-			for (int j = i + 1; j < maxSteps; j++) {
-				double[] sigmas1 = new double[(int) maxSteps];
-				double[] sigmas2 = new double[(int) maxSteps];
-				Arrays.fill(sigmas1, Math.pow(2, i) * minSigma);
-				Arrays.fill(sigmas2, Math.pow(2, j) * minSigma);
-				// FIXME Nullpointer in DoGVaryingSigmas.conforms()
-				// in() is not initialized in conforms method, because no input is given
-				ComputerOp<RandomAccessibleInterval, RandomAccessibleInterval> tempOp = ops().computer(
-						Ops.Filter.DoG.class, RandomAccessibleInterval.class, RandomAccessibleInterval.class, sigmas1,
-						sigmas2);
+			for (int j = i + 1; j <= maxSteps; j++) {
+				FunctionOp<RandomAccessibleInterval<T>, RandomAccessibleInterval> tempOp = ops()
+						.function(Ops.Filter.DoG.class,
+								RandomAccessibleInterval.class, extendedIn,
+								Math.pow(2, i) * minSigma,
+								Math.pow(2, j) * minSigma);
 				doGOps.add(tempOp);
 			}
 		}
@@ -64,13 +63,17 @@ public class DoGPixelFeature<T extends RealType<T>> extends AbstractPixelFeature
 
 	@Override
 	public RandomAccessibleInterval<T> compute(RandomAccessibleInterval<T> input) {
-		RandomAccessibleInterval<T> extendedIn = Views.interval(Views.extendMirrorDouble(input), input);
-		int i = 0;
-		for (ComputerOp<RandomAccessibleInterval, RandomAccessibleInterval> doGOp : doGOps) {
 
-			RandomAccessibleInterval<T> outSlice = Views.hyperSlice(Views.hyperSlice(output, 3, 1), 2, i);
-			// FIXME no subtract method for DoG found
-			doGOp.compute(input, outSlice);
+		int i = 0;
+		for (FunctionOp<RandomAccessibleInterval<T>, RandomAccessibleInterval> doGOp : doGOps) {
+
+			RandomAccessibleInterval<T> outSlice = Views.hyperSlice(
+					Views.hyperSlice(output, 3, 1), 2, i);
+			RandomAccessibleInterval tempOut = doGOp.compute(extendedIn);
+			// FIXME copy tempOut into outSlice
+			// throws ArrayIndexOutOfBoundsException after 3rd iteration
+			ops().copy().rai(outSlice, tempOut);
+			
 			i++;
 		}
 		return output;
