8106ba657aed777ee3c6ab13c1d6c991a2cd81bf
Eike Heinz <eike.w.heinz@gmail.com>
2015-12-09 14:24:22 +0100

    wip gaussian gradient magnitude

--- src/main/java/net/imagej/ops/image/pixelfeature/GaussianGradientMagnitudePixelFeature.java
+++ src/main/java/net/imagej/ops/image/pixelfeature/GaussianGradientMagnitudePixelFeature.java
@@ -9,6 +9,9 @@ import net.imagej.ops.Ops.Image.GaussianGradientMagnitudePxFeature;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.gauss3.SeparableSymmetricConvolution;
 import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.Views;
 
@@ -17,9 +20,8 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 @Plugin(type = Ops.Image.GaussianGradientMagnitudePxFeature.class, name = Ops.Image.GaussianGradientMagnitudePxFeature.NAME)
-public class GaussianGradientMagnitudePixelFeature<T extends RealType<T>>
-		extends AbstractPixelFeatureOp<T> implements
-		GaussianGradientMagnitudePxFeature {
+public class GaussianGradientMagnitudePixelFeature<T extends RealType<T>> extends AbstractPixelFeatureOp<T>
+		implements GaussianGradientMagnitudePxFeature {
 
 	@Parameter(type = ItemIO.INPUT)
 	private double sigma;
@@ -30,41 +32,50 @@ public class GaussianGradientMagnitudePixelFeature<T extends RealType<T>>
 
 		RandomAccessibleInterval<T> output1 = ops().create().img(input);
 		RandomAccessibleInterval<T> output2 = ops().create().img(input);
+
+		// FIXME hardcoded for testing purpose - kernels might not be correct
 		double[][] kernelX = new double[][] { { 1, 2, 1 }, { -1, 0, 1 } };
 		double[][] kernelY = new double[][] { { -1, 0, 1 }, { 1, 2, 1 } };
 
-		// FIXME blocking queue size, threads
-		int numThreads = Runtime.getRuntime().availableProcessors();
-		ThreadPoolExecutor pool = new ThreadPoolExecutor(numThreads,
-				numThreads, 0L, TimeUnit.MILLISECONDS,
-				new ArrayBlockingQueue<Runnable>(32000));
+		RandomAccessibleInterval<T> extended = Views.interval(Views.extendMirrorSingle(input), input);
+		// smoothing image
+		RandomAccessibleInterval<T> blurred = ops().filter().gauss(extended, sigma);
 
-		RandomAccessibleInterval<T> extended = Views.interval(
-				Views.extendMirrorSingle(input), input);
-		RandomAccessibleInterval<T> blurred = ops().filter().gauss(extended,
-				sigma);
+		ThreadPoolExecutor pool = null;
 		try {
+			// FIXME blocking queue size, threads, both instances
+			int numThreads = Runtime.getRuntime().availableProcessors();
+			pool = new ThreadPoolExecutor(numThreads, numThreads, 0L, TimeUnit.MILLISECONDS,
+					new ArrayBlockingQueue<Runnable>(32000));
 
-			SeparableSymmetricConvolution.convolve(kernelX, blurred, output1,
-					pool);
-			ops().map(output1, output1,
-					ops().op(Ops.Math.Sqr.class, RealType.class, RealType.class));
-			SeparableSymmetricConvolution.convolve(kernelY, blurred, output2,
-					pool);
-			ops().map(output2, output2,
-					ops().op(Ops.Math.Sqr.class, RealType.class, RealType.class));
-			ops().math().add(output, output1);
-			ops().math().add(output, output2);
-			ops().map(output, output,
-					ops().op(Ops.Math.Sqrt.class, RealType.class,
-							RealType.class));
+			ImageJFunctions.show(blurred, "begin");
+			// convolve the image with both sobel filters
+			// FIXME both convolution calls throw
+			// ArrayIndexOutOfBoundsExceptions
+			SeparableSymmetricConvolution.convolve(kernelX, blurred, output1, pool);
+			pool.shutdown();
+			ImageJFunctions.show(output1, "out1");
+			pool = new ThreadPoolExecutor(numThreads, numThreads, 0L, TimeUnit.MILLISECONDS,
+					new ArrayBlockingQueue<Runnable>(32000));
+			SeparableSymmetricConvolution.convolve(kernelY, blurred, output2, pool);
+			ImageJFunctions.show(output2, "out2");
+
+			pool.shutdown();
+
+			// calculate gradient magnitude in each pixel G = sqrt(Gx^2 + Gy^2)
+			ops().map(output1, output1, ops().op(Ops.Math.Sqr.class, RealType.class, RealType.class));
+			ops().map(output2, output2, ops().op(Ops.Math.Sqr.class, RealType.class, RealType.class));
+
+			output = (RandomAccessibleInterval<T>) ops().math().add(output, output1);
+			output = (RandomAccessibleInterval<T>) ops().math().add(output, output2);
+			ops().map(output, output, ops().op(Ops.Math.Sqrt.class, RealType.class, RealType.class));
 
 		} catch (IncompatibleTypeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		return output1;
+		return output;
 	}
 
 }
--- src/test/java/net/imagej/ops/image/pixelfeatures/GaussianGradientMagnitudePxFeatureTest.java
+++ src/test/java/net/imagej/ops/image/pixelfeatures/GaussianGradientMagnitudePxFeatureTest.java
@@ -1,5 +1,7 @@
 package net.imagej.ops.image.pixelfeatures;
 
+import org.junit.Test;
+
 import net.imagej.ops.AbstractOpTest;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
@@ -8,28 +10,34 @@ import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 
-import org.junit.Test;
-
 public class GaussianGradientMagnitudePxFeatureTest extends AbstractOpTest {
 
 	@Test
 	public <T extends RealType<T>> void test() {
+		
+		
+		
 		Img<FloatType> img = generateFloatArrayTestImg(false, new long[] {
 				500, 500 });
 
-		Cursor<FloatType> cursor = img.cursor();
-		long counter = 0;
-		while (cursor.hasNext()) {
-			if (counter > 225000 || counter < 65000) {
-				cursor.next().setZero();
-			} else if (counter >= 65000) {
-				cursor.next().set(300);
+		Cursor<FloatType> cursorImg = img.cursor();
+		int counterX = 0;
+		int counterY = 0;
+		while (cursorImg.hasNext()) {
+			if(counterX > 240 && counterX < 260 || counterY > 120000 && counterY < 130000) {
+				cursorImg.next().set(255);
+			} else {
+			cursorImg.next().setZero();
 			}
-			counter++;
+			counterX++;
+			counterY++;
+			if(counterX == 500) {
+				counterX =0;
+			}
 		}
-			
 		ImageJFunctions.show(img, "input");
-		RandomAccessibleInterval<T> out = ops.image().gaussianGradientMagnitude((RandomAccessibleInterval<T>) img, 1.0d);
+		System.out.println("breakpoint");
+		RandomAccessibleInterval<T> out = ops.image().gaussianGradientMagnitude((RandomAccessibleInterval<T>) img, 16.0d);
 				
 		ImageJFunctions.show(out, "output");
 		System.out.println("breakpoint");
