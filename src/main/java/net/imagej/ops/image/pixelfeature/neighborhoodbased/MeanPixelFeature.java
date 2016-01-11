fa2c5aafa42905a22871c3989b6bfad75f1e07a9
Eike Heinz <eike.w.heinz@gmail.com>
2016-01-11 15:59:03 +0100

    cleanup stat neighborhoodbased features

--- src/main/java/net/imagej/ops/image/pixelfeature/neighborhoodbased/MeanPixelFeature.java
+++ src/main/java/net/imagej/ops/image/pixelfeature/neighborhoodbased/MeanPixelFeature.java
@@ -3,13 +3,10 @@ package net.imagej.ops.image.pixelfeature.neighborhoodbased;
 import org.scijava.plugin.Plugin;
 
 import net.imagej.ops.Ops;
-import net.imagej.ops.Ops.Create;
 import net.imagej.ops.Ops.Filter.Mean;
 import net.imagej.ops.Ops.Image.MeanPxFeature;
 import net.imagej.ops.special.Computers;
-import net.imagej.ops.special.Functions;
 import net.imagej.ops.special.UnaryComputerOp;
-import net.imagej.ops.special.UnaryFunctionOp;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.neighborhood.RectangleShape;
 import net.imglib2.type.numeric.RealType;
@@ -19,22 +16,22 @@ import net.imglib2.view.Views;
 public class MeanPixelFeature<T extends RealType<T>> extends AbstractNeighborhoodPixelFeatureOp<T>
 		implements MeanPxFeature {
 
-	private UnaryFunctionOp<RandomAccessibleInterval, RandomAccessibleInterval> createOp;
-	private UnaryComputerOp<RandomAccessibleInterval, RandomAccessibleInterval> mapOp;
+//	private UnaryFunctionOp<RandomAccessibleInterval, RandomAccessibleInterval> createOp;
+	private UnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> mapOp;
+	private RandomAccessibleInterval<T> output;
 
 	@Override
 	public void initialize() {
-		createOp = Functions.unary(ops(), Create.Img.class, RandomAccessibleInterval.class,
-				RandomAccessibleInterval.class);
-		mapOp = Computers.unary(ops(), Mean.class, RandomAccessibleInterval.class, RandomAccessibleInterval.class,
+		output = ops().create().img(in());
+		mapOp = Computers.unary(ops(), Mean.class, output, in(),
 				new RectangleShape(span, false));
 	}
 
 	@Override
 	public RandomAccessibleInterval<T> compute1(RandomAccessibleInterval<T> in) {
-		final RandomAccessibleInterval<T> out = createOp.compute1(in);
-		mapOp.compute1(Views.interval(Views.extendMirrorDouble(in), in), out);
-		return out;
+//		final RandomAccessibleInterval<T> out = createOp.compute1(in);
+		mapOp.compute1(Views.interval(Views.extendMirrorDouble(in), in), output);
+		return output;
 	}
 
 }
--- src/main/java/net/imagej/ops/image/pixelfeature/neighborhoodbased/MinPixelFeature.java
+++ src/main/java/net/imagej/ops/image/pixelfeature/neighborhoodbased/MinPixelFeature.java
@@ -3,13 +3,10 @@ package net.imagej.ops.image.pixelfeature.neighborhoodbased;
 import org.scijava.plugin.Plugin;
 
 import net.imagej.ops.Ops;
-import net.imagej.ops.Ops.Create;
 import net.imagej.ops.Ops.Filter.Min;
 import net.imagej.ops.Ops.Image.MinPxFeature;
 import net.imagej.ops.special.Computers;
-import net.imagej.ops.special.Functions;
 import net.imagej.ops.special.UnaryComputerOp;
-import net.imagej.ops.special.UnaryFunctionOp;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.neighborhood.RectangleShape;
 import net.imglib2.type.numeric.RealType;
@@ -19,22 +16,20 @@ import net.imglib2.view.Views;
 public class MinPixelFeature<T extends RealType<T>> extends AbstractNeighborhoodPixelFeatureOp<T>
 		implements MinPxFeature {
 
-	private UnaryFunctionOp<RandomAccessibleInterval, RandomAccessibleInterval> createOp;
-	private UnaryComputerOp<RandomAccessibleInterval, RandomAccessibleInterval> mapOp;
+	private UnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> mapOp;
+	private RandomAccessibleInterval<T> output;
 
 	@Override
 	public void initialize() {
-		createOp = Functions.unary(ops(), Create.Img.class, RandomAccessibleInterval.class,
-				RandomAccessibleInterval.class);
-		mapOp = Computers.unary(ops(), Min.class, RandomAccessibleInterval.class, RandomAccessibleInterval.class,
+		output = ops().create().img(in());
+		mapOp = Computers.unary(ops(), Min.class, output, in(),
 				new RectangleShape(span, false));
 	}
 
 	@Override
 	public RandomAccessibleInterval<T> compute1(RandomAccessibleInterval<T> in) {
-		final RandomAccessibleInterval<T> out = createOp.compute1(in);
-		mapOp.compute1(Views.interval(Views.extendMirrorDouble(in), in), out);
-		return out;
+		mapOp.compute1(Views.interval(Views.extendMirrorDouble(in), in), output);
+		return output;
 	}
 
 }
