fab4d0cef8e5f0000809846fe4b5104a7c18148d
Eike Heinz <eike.w.heinz@gmail.com>
2016-01-20 17:38:08 +0100

    update hessian feature

--- src/main/java/net/imagej/ops/image/pixelfeature/HessianPixelFeatureOp.java
+++ src/main/java/net/imagej/ops/image/pixelfeature/HessianPixelFeatureOp.java
@@ -5,13 +5,16 @@ import org.scijava.plugin.Plugin;
 import net.imagej.ops.Ops;
 import net.imagej.ops.Ops.Math.Add;
 import net.imagej.ops.Ops.Math.Sqrt;
+import net.imagej.ops.special.BinaryComputerOp;
 import net.imagej.ops.special.Computers;
 import net.imagej.ops.special.Functions;
 import net.imagej.ops.special.UnaryComputerOp;
 import net.imagej.ops.special.UnaryFunctionOp;
 import net.imglib2.Dimensions;
 import net.imglib2.FinalDimensions;
+import net.imglib2.IterableInterval;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.util.Util;
 import net.imglib2.view.IntervalView;
@@ -27,12 +30,12 @@ public class HessianPixelFeatureOp<T extends RealType<T>> extends AbstractPixelF
 	@SuppressWarnings("rawtypes")
 	private UnaryFunctionOp<RandomAccessibleInterval<T>, RandomAccessibleInterval> convolverKernelY;
 	private UnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> sqrtMapOp;
+	private UnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> addMapOp;
 
 	@Override
 	public void initialize() {
 		// TODO get dimensionality from input
-		RandomAccessibleInterval<T> sobelKernel = (RandomAccessibleInterval<T>) ops().create()
-				.kernelSobel(new double[] { 3, 3 });
+		RandomAccessibleInterval<T> sobelKernel = ops().create().kernelSobel(in().numDimensions());
 		RandomAccessibleInterval<T> kernelX = Views.hyperSlice(Views.hyperSlice(sobelKernel, 3, 0), 2, 0);
 		RandomAccessibleInterval<T> kernelY = Views.hyperSlice(Views.hyperSlice(sobelKernel, 3, 0), 2, 1);
 		convolverKernelX = Functions.unary(ops(), Ops.Filter.Convolve.class, RandomAccessibleInterval.class, in(),
@@ -41,13 +44,15 @@ public class HessianPixelFeatureOp<T extends RealType<T>> extends AbstractPixelF
 				kernelY);
 		// Computers.unary(ops(), Ops.Math.Add.class,
 		// RandomAccessibleInterval.class, in());
-		Add addOp = ops().op(Ops.Math.Add.class, RealType.class, RealType.class);
+		//Add addOp = ops().op(Ops.Math.Add.class, RealType.class, RealType.class);
 		// addMapOp = Computers.unary(ops(), Ops.Map.class,
 		// RandomAccessibleInterval.class, RandomAccessibleInterval.class,
 		// addOp);
 
 		Sqrt sqrtOp = ops().op(Ops.Math.Sqrt.class, RealType.class, RealType.class);
 		sqrtMapOp = Computers.unary(ops(), Ops.Map.class, in(), in(), sqrtOp);
+		Add addOp = ops().op(Ops.Math.Add.class, RealType.class, RealType.class, RealType.class);
+		addMapOp = (UnaryComputerOp) Computers.unary(ops(), Ops.Map.class, RandomAccessibleInterval.class, RandomAccessibleInterval.class, addOp);
 
 	}
 
@@ -69,12 +74,10 @@ public class HessianPixelFeatureOp<T extends RealType<T>> extends AbstractPixelF
 		/*
 		 * output px value in hessian matrix is: [[XX,XY],[YX,YY]] =
 		 * 
-		 * [[a,b],[c,d]] 
-		 * Trace T=a+d 
-		 * Determinant D=ad-bc
+		 * [[a,b],[c,d]] Trace T=a+d Determinant D=ad-bc
 		 * 
-		 * First eigenvalue: (T/2) + sqrt((4b^2+(a-d)^2)/2)
-		 * Second eigenvalue: (T/2) - sqrt((4b^2+(a-d)^2)/2)
+		 * First eigenvalue: (T/2) + sqrt((4b^2+(a-d)^2)/2) Second eigenvalue:
+		 * (T/2) - sqrt((4b^2+(a-d)^2)/2)
 		 */
 
 		// create output img containing slice for each feature
@@ -95,6 +98,8 @@ public class HessianPixelFeatureOp<T extends RealType<T>> extends AbstractPixelF
 
 		// calculate trace
 		RandomAccessibleInterval<T> trace = (RandomAccessibleInterval<T>) ops().math().add(convolveXX, convolveYY);
+		
+		//ImageJFunctions.show(traceSlice);
 		ops().copy().rai(traceSlice, trace);
 
 		// calculate determinant
