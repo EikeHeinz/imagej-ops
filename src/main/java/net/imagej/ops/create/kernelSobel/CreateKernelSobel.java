93cb15987030bdd3bf5c70a3685027fac87d4034
Eike Heinz <eike.w.heinz@gmail.com>
2016-01-21 15:53:27 +0100

    implement sobel kernel

--- src/main/java/net/imagej/ops/create/kernelSobel/CreateKernelSobel.java
+++ src/main/java/net/imagej/ops/create/kernelSobel/CreateKernelSobel.java
@@ -5,9 +5,7 @@ import org.scijava.plugin.Plugin;
 import net.imagej.ops.Ops;
 import net.imagej.ops.create.AbstractCreateSobelKernel;
 import net.imglib2.Cursor;
-import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgFactory;
-import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.ComplexType;
 import net.imglib2.type.numeric.real.DoubleType;
@@ -15,57 +13,30 @@ import net.imglib2.type.numeric.real.DoubleType;
 @Plugin(type = Ops.Create.KernelSobel.class, name = Ops.Create.KernelSobel.NAME)
 public class CreateKernelSobel<T extends ComplexType<T> & NativeType<T>> extends AbstractCreateSobelKernel<T> {
 
-	// TODO fix inputs maybe create new abstract kernel class for sobel
-	// TODO fix type
 
 	@Override
 	protected void createKernel() {
-		
-//		long[] dim = new long[dimensionality];
-//
-//
-//		dim[0] = 3;
-//		dim[1] = 1;
-//
-//		for (int k = 2; k < dim.length; k++)
-//			dim[k] = 1;
-//		
-//		Img<DoubleType> h = ArrayImgs.doubles(new double[] { 1, 2, 1 }, dim);
-//		Img<DoubleType> hp = ArrayImgs.doubles(new double[] { -1, 0, 1 }, dim);
 
-		// case: 2d
-		if (numDimensions == 2) {
+		long[] dim = new long[numDimensions + 2];
 
-			int[] valuesX = new int[] { 1, 2, 1, 0, 0, 0, -1, -2, -1 };
-			// TODO remove y values and rotate x to obtain y-direction
-			int[] valuesY = new int[] { -1, 0, 1, -2, 0, 2, -1, 0, 1 };
+		dim[0] = 3;
+		dim[1] = 1;
 
-			createOutputImg(new long[] { 3, 3, 1, 2 }, getFac(), getOutType(), new ArrayImgFactory<DoubleType>(),
-					new DoubleType());
-
-			final Cursor<T> cursor = getOutput().cursor();
-			int i = 0;
-			while (cursor.hasNext()) {
-				cursor.fwd();
-				if (i < 9) {
-					cursor.get().setReal(valuesX[i]);
-				} else {
-					int pos = i - 9;
-					cursor.get().setReal(valuesY[pos]);
-				}
-				i++;
-			}
-			// RandomAccessibleInterval<T> test = Views.rotate(getOutput(), 0,
-			// 1);
-			// ImageJFunctions.show(test);
-			// System.out.println("breakpoint");
-
-		} else if (numDimensions == 3) {
-			// implement
-			createOutputImg(new long[] { 3, 3, 3, 1, 3 }, getFac(), getOutType(), new ArrayImgFactory<DoubleType>(),
-					new DoubleType());
+		for (int k = 2; k < dim.length; k++) {
+			dim[k] = 1;
 		}
 
+		dim[dim.length - 1] = 2;
+
+		createOutputImg(dim, getFac(), getOutType(), new ArrayImgFactory<DoubleType>(), new DoubleType());
+		final Cursor<T> cursor = getOutput().cursor();
+		int i = 0;
+		float[] values = { 1.0f, 2.0f, 1.0f, -1.0f, 0.0f, 1.0f };
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			cursor.get().setReal(values[i]);
+			i++;
+		}
 	}
 
 }
