b2dd5b744f8d5659d3921b7686180fe70b1bef47
Eike Heinz <eike.w.heinz@gmail.com>
2016-01-21 16:13:42 +0100

    gaussian gradient now uses sobel op

--- src/main/java/net/imagej/ops/image/pixelfeature/GaussianGradientMagnitudePixelFeature.java
+++ src/main/java/net/imagej/ops/image/pixelfeature/GaussianGradientMagnitudePixelFeature.java
@@ -7,11 +7,9 @@ import org.scijava.plugin.Plugin;
 
 import net.imagej.ops.Ops;
 import net.imagej.ops.Ops.Image.GaussianGradientMagnitudePxFeature;
-import net.imagej.ops.Ops.Math.Sqr;
-import net.imagej.ops.Ops.Math.Sqrt;
 import net.imagej.ops.special.computer.Computers;
-import net.imagej.ops.special.function.Functions;
 import net.imagej.ops.special.computer.UnaryComputerOp;
+import net.imagej.ops.special.function.Functions;
 import net.imagej.ops.special.function.UnaryFunctionOp;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.RealType;
@@ -28,37 +26,15 @@ public class GaussianGradientMagnitudePixelFeature<T extends RealType<T>> extend
 
 	private UnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> gaussOp;
 
-	private UnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> convolverKernelX;
-
-	private UnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> convolverKernelY;
-
-	private UnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> squareMapOp;
-
-	private UnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<T>> sqrtMapOp;
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public void initialize() {
 		createOp = (UnaryFunctionOp) Functions.unary(ops(), Ops.Create.Img.class, RandomAccessibleInterval.class,
 				in() != null ? in() : RandomAccessibleInterval.class);
-
-		// TODO get dimensionality from input
-		RandomAccessibleInterval<T> sobelKernel = ops().create().kernelSobel(in().numDimensions());
-		RandomAccessibleInterval<T> kernelX = Views.hyperSlice(Views.hyperSlice(sobelKernel, 3, 0), 2, 0);
-		RandomAccessibleInterval<T> kernelY = Views.hyperSlice(Views.hyperSlice(sobelKernel, 3, 0), 2, 1);
-
 		double[] sigmas = new double[in().numDimensions()];
 		Arrays.fill(sigmas, sigma);
 		gaussOp = Computers.unary(ops(), Ops.Filter.Gauss.class, in(), in(), sigmas);
-
-		convolverKernelX = Computers.unary(ops(), Ops.Filter.Convolve.class, in(), in(), kernelX);
-		convolverKernelY = Computers.unary(ops(), Ops.Filter.Convolve.class, in(), in(), kernelY);
-
-		Sqr squareOp = ops().op(Ops.Math.Sqr.class, RealType.class, RealType.class);
-		squareMapOp = Computers.unary(ops(), Ops.Map.class, in(), in(), squareOp);
-		Sqrt sqrtOp = ops().op(Ops.Math.Sqrt.class, RealType.class, RealType.class);
-		sqrtMapOp = Computers.unary(ops(), Ops.Map.class, in(), in(), sqrtOp);
-
 	}
 
 	@Override
@@ -69,22 +45,8 @@ public class GaussianGradientMagnitudePixelFeature<T extends RealType<T>> extend
 		RandomAccessibleInterval<T> blurred = createOp.compute1(input);
 		gaussOp.compute1(extended, blurred);
 
-		RandomAccessibleInterval<T> convolveX = createOp.compute1(input);
-		RandomAccessibleInterval<T> convolveY = createOp.compute1(input);
-		convolverKernelX.compute1(blurred, convolveX);
-		convolverKernelY.compute1(blurred, convolveY);
+		RandomAccessibleInterval<T> output = ops().filter().sobel(blurred);
 
-		// calculate gradient magnitude in each pixel G = sqrt(Gx^2 + Gy^2)
-		squareMapOp.compute1(convolveX, convolveX);
-		squareMapOp.compute1(convolveY, convolveY);
-
-		RandomAccessibleInterval<T> output = createOp.compute1(input);
-
-		// FIXME create Op in initialize method
-		output = (RandomAccessibleInterval<T>) ops().math().add(output, convolveX);
-		output = (RandomAccessibleInterval<T>) ops().math().add(output, convolveY);
-
-		sqrtMapOp.compute1(output, output);
 		return output;
 	}
 
